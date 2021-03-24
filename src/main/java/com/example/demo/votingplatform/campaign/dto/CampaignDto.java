349ce100688acd643532c7dd53d9f133a0b2b430
AlexCatalin21 <alexandru.g2101@gmail.com>
2021-03-24 19:27:46 +0200

    added candidate and topic dto lists to campaign dto

--- src/main/java/com/example/demo/votingplatform/campaign/dto/CampaignDto.java
+++ src/main/java/com/example/demo/votingplatform/campaign/dto/CampaignDto.java
@@ -1,11 +1,17 @@
 package com.example.demo.votingplatform.campaign.dto;
 
+import com.example.demo.votingplatform.candidates.dto.CandidateDto;
+import com.example.demo.votingplatform.topics.dto.TopicDto;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class CampaignDto {
-    private String name,description, campaignTypeId;
+    private String name,description, campaignTypeId,password, confirmedPassword;
     private Date startDate, expireDate;
+    private Long ownerUserId;
+    private List<CandidateDto> candidateDtoList;
+    private List<TopicDto> topicDtoList;
 }
